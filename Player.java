0f6f0b06ed5e68afde3bb2ac367e08fba71db032
jackxuuuuuu <wy_xuyunjie@163.com>
2026-02-09 15:27:28 +0800

    Update Player.java to include player class with name and symbol

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player {
+    private String name;
+    private char symbol; // 'X' or 'O'
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        if(symbol == 'X' || symbol == 'O') {
+            this.symbol = symbol;
+        } else {
+            throw new IllegalArgumentException("Symbol must be either 'X' or 'O'.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
